48747dbc392784f4c401289eeef7935d167aac22
Sumit Ghewade <ghewadesumit@gmail.com>
2023-03-27 15:37:12 -0400

    added colorfull icons

--- client/src/common/constants/index.js
+++ client/src/common/constants/index.js
@@ -1,16 +1,25 @@
-import {FaReact,FaNodeJs} from 'react-icons/fa';
-import {DiJavascript1,DiCss3,DiMysql} from 'react-icons/di';
-import {SiRedux,SiMongodb} from 'react-icons/si';
+
+import css3 from '../../assets/img/css3.svg';
+import git from '../../assets/img/git.svg';
+import react from '../../assets/img/react.svg';
+import angular from '../../assets/img/angular.svg';
+import javascript from '../../assets/img/javascript.svg';
+import typescript from '../../assets/img/typescript.svg';
+import node from '../../assets/img/nodejs.svg';
+import mysql from '../../assets/img/mysql.svg';
+import redux from '../../assets/img/redux.svg';
 
 export const skills = [
-    {skillName:'React',icon:<FaReact size={50}/>,rating:4.5,percentage:70},
-    {skillName:'Node.js', icon: <FaNodeJs size={50}/>,rating:3,percentage:50},
-    {skillName:'JavaScript',icon:<DiJavascript1 size={50}/>,rating:4.5,percentage:80},
-    {skillName:'Redux', icon:<SiRedux size={50}/>,rating:3.5,percentage:60},
-    {skillName:'CSS',icon:<DiCss3 size={50}/>,rating:4,percentage:60},
-    {skillName:'MongoDB',icon:<SiMongodb size={50}/>,rating:3,percentage:50},
-    {skillName:'MySql',icon:<DiMysql size={50}/>,rating:3,percentage:50},]
+    {skillName:'React',icon:<img alt="react" src={react}/>,rating:4.5,percentage:70},
+    {skillName:'Angular',icon:<img alt="angular" src={angular}/>,rating:3,percentage:70},
+    {skillName:'Typescript', icon: <img alt="typescript" src={typescript}/>,rating:3,percentage:50},
+    {skillName:'JavaScript',icon:<img alt="javascript" src={javascript}/>,rating:4.5,percentage:80},
+    {skillName:'Redux', icon:<img alt="redux" src={redux}/>,rating:3.5,percentage:60},
+    {skillName:'CSS',icon:<img alt="css3" src={css3}/>,rating:4,percentage:60},
+    {skillName:'Node',icon:<img alt="node" src={node}/>,rating:3,percentage:50},
+    {skillName:'MySql',icon:<img alt="mysql" src={mysql}/>,rating:3,percentage:50},
+    {skillName:'Git',icon:<img alt="git" src={git}/>,rating:3.5,percentage:50},]
 
     export const projectDescription = 'These projects where built while I was learning new tech stack and making myself familiar with it. Some project links do actually work so check them out.';
     export const skillDescription = 'Technology stack that I have learnt and used while sailing the path of programming. Feel free to ask me any doubt regarding the below mentioned stack and hopefully I would be able to answer them.';
-    export const aboutme = `Graduated with a Master's Degree in Computer Science from the University of Cincinnati. Have 4+ years of professional experience as a Full-stack developer. I love to play around UI and like solving Data structure and algorithm challenges. When I'm not working I will be either Gaming, Traveling, Filming, Gyming,.. you get the idea :)`;
+    export const aboutme = `Graduated with a Master's Degree in Computer Science from the University of Cincinnati. Have 5+ years of professional experience as a Full-stack developer. I love to play around UI and like solving Data structure and algorithm challenges. When I'm not working I will be either Gaming, Traveling, Filming, Gyming,.. you get the idea :)`;
